68bd88e01d3615731051d1ab8c688d3886ca043f
kirillz <kzasorin@gmail.com>
2022-03-12 11:32:32 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# react-redux-app
+
+ammer_and_wrench: Установка
+
+- установите [NodeJS](https://nodejs.org/en/) (если требуется) и [Yarn](https://yarnpkg.com/en/docs/install)
+- скачайте сборку с помощью [Git](https://git-scm.com/downloads): `git clone https://github.com/kirillz/react-redux-app.git`
+- перейдите в скачанную папку со сборкой: `cd react-redux-app`
+- скачайте необходимые зависимости: `yarn`
+- чтобы начать работу, введите команду: `yarn start` (режим разработки)
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: "[name].[hash].js"
+    },
+    devServer: {
+        port: 3000
+    },
+    plugins: [
+            new HTMLWebpackPlugin({template:'./src/index.html'}),
+            new CleanWebpackPlugin()
+        ],
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
+            {
+                test: /\.(jpg|jpeg|png|svg)$/i,
+                use: ["file-loader"]
+
+            }
+        ]
+    }
+}
